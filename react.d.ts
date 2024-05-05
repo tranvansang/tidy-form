497c7e30e0fb3c0963b6d28b09df9baf12a7be34
Sang Tran <t@sang.jp>
2024-05-06 04:25:18 +0900

    del

--- react.d.ts
+++
@@ -1,5 +0,0 @@
-import type { Get } from 'type-fest';
-import type { FormControl } from './index.ts';
-export declare function useFormControl<T>(defaultValue?: T | (() => T)): FormControl<T>;
-export declare function useFormValue<T>(form: FormControl<T>): T;
-export declare function useFormValue<T, V extends string>(form: FormControl<T>, field: V): Get<T, V>;
